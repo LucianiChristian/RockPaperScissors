1f4af898ce0ddf641d17b887d9c87cbeaf0ea308
LucianiChristian <luciani_christian@yahoo.com>
2022-05-09 07:42:09 -0500

    All work was done off machine.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,65 @@
+/* Game Values */
+let userWinCount = 0;
+let computerWinCount = 0;
+let roundNumber = 0;
+
+
+
+/* Switch To In Play Display */
+function transitionInPlay() {  
+    const startMenu = document.querySelector(".start-menu");
+    const inPlay = document.querySelector(".in-play");
+
+    startMenu.style.display = "none";
+    inPlay.style.display = "flex";
+}
+
+/* Executes The Turn And Returns The Round Winner */
+function round(userChoice) {
+    // Computer Takes It's Turn
+    let computerChoice = computerTurn();
+    // Who Wins?
+    let winner = roundWinner(userChoice, computerChoice);
+    // Increment Win Count & Modify Scoreboard
+    const playerScoreDisplay = document.querySelector(".player-score");
+    const computerScoreDisplay = document.querySelector(".computer-score");
+    const roundNumberDisplay = document.querySelector(".computer-choice-text");
+    const scoreboardDisplay = document.querySelector(".scoreboard");
+    const userChoiceImageDisplay = document.querySelector(".player-choice-image");
+    const computerChoiceImageDisplay = document.querySelector(".computer-choice-image");
+    const choseDisplay = document.querySelector(".choice");
+
+    userChoiceImageDisplay.setAttribute('src', 'images/' + userChoice + '.jpg');
+    computerChoiceImageDisplay.setAttribute('src', 'images/' + computerChoice + '.jpg');
+
+    if (winner === "user") {
+        userWinCount++;
+        roundNumber++;
+        playerScoreDisplay.innerHTML = "Player Score: " + userWinCount;
+    }
+    else if (winner === "computer") {
+        computerWinCount++;
+        roundNumber++;
+        computerScoreDisplay.innerHTML = "Computer Score: " + computerWinCount;
+    }
+
+    roundNumberDisplay.innerHTML = "Round: " + roundNumber;
+
+    console.log("User Choice -> " + userChoice+ "\nComputer Choice -> " + computerChoice + "\nWinner : " + winner);
+    console.log("\nuserWinCount : " + userWinCount + "\ncomputerWinCount : " + computerWinCount);
+
+    if(userWinCount === 5){
+        scoreboardDisplay.innerHTML = '<div class="game-status"><h3 class="game-status-text">You Won The Game!</h3><button class="try-again" onclick="resetGame()">Try Again?</button></div>';
+        choseDisplay.style.display = "none";
+    }
+    if(computerWinCount === 5){
+        scoreboardDisplay.innerHTML = '<div class="game-status"><h3 class="game-status-text">You lost...</h3><button class="try-again" onclick="resetGame()">Try Again?</button></div>';
+        choseDisplay.style.display = "none";
+    }
+
+}
+
+/* Takes A Turn For The Computer */
 function computerTurn() {
     let computerChoice = Math.random();
 
@@ -11,75 +73,44 @@ function computerTurn() {
         return "scissors";
     }
 }
-function userTurn() {
-    let userInput = prompt("Rock, paper, or scissors?");
 
-    return userInput.toLowerCase();
-}
-
-
-function playRound() {
-    let computerChoice = computerTurn();
-    let userChoice = userTurn();
-
-    if(userChoice === computerChoice) {
-        return "Its a draw! Try again";
+/* Computes The Winner Based On User-Computer Choices */
+function roundWinner(userChoice, computerChoice) {
+    if(userChoice === computerChoice) 
+    {
+        return "draw";
     }
-    else if(userChoice === "rock" && computerChoice === "paper" ||){
-        return "You win! Rock beats Paper!";
+    else if(userChoice === "rock" && computerChoice === "scissors" || 
+            userChoice === "paper" && computerChoice === "rock" || 
+            userChoice === "scissors" && computerChoice === "paper")
+    {
+        return "user";
+    }   
+    else if(computerChoice === "rock" && userChoice === "scissors" ||
+            computerChoice === "paper" && userChoice === "rock" ||
+            computerChoice === "scissors" && userChoice === "paper")
+    {
+        return "computer";
     }
-    else if(userChoice === "paper" && computerChoice === "rock"){
-        return "You win! Paper beats Rock!";
-    }
-    else if(userChoice === "scissors" && computerChoice === "paper"){
-        return "You win! Scissors beats Paper!";
-    }
-    else if(computerChoice === "rock" && userChoice === "paper"){
-        return "You lose! Rock beats Paper!";
-    }
-    else if(computerChoice === "paper" && userChoice === "rock"){
-        return "You lose! Paper beats Rock!";
-    }
-    else if(computerChoice === "scissors" && userChoice === "paper"){
-        return "You lose! Scissors beats Paper!";
-    }
-    else {
-        return "Incorrect input! Try again.";
+    else 
+    {
+        return "error";
     }
 }
 
-function game() {
-    let userWinCount = 0;
-    let computerWinCount = 0;
+/* Resets The In Play Game State */
+function resetGame() {
+    const scoreboardDisplay = document.querySelector(".scoreboard");
+    const computerChoiceTextDisplay = document.querySelector(".computer-choice-text");
+    const choseDisplay = document.querySelector(".choice");
 
-    for(let i = 0; i < 5; i++){
-        let consoleMessage = playRound();
-
-        if(consoleMessage === "Incorrect input! Try again.") {
-            i--;
-        }
-
-        if(consoleMessage.includes("You win!")){
-            userWinCount++;
-        }
-        else if(consoleMessage.includes("You lose!")){
-            computerWinCount++;
-        }
-
-        console.log(consoleMessage);
-    }
-
-    if(userWinCount > computerWinCount) {
-        console.log("You've won!");
-    }
-    else if (computerWinCount > userWinCount) {
-        console.log("You've lost!");
-    }
-    else {
-        console.log("It's a tie!");
-    }
+    userWinCount = 0;
+    computerWinCount = 0;
+    roundNumber = 0;
+    scoreboardDisplay.innerHTML = '<h3 class="player-score">Player Score: 0</h3><h3 class="computer-score">Computer Score: 0</h3>';
+    computerChoiceTextDisplay.innerHTML = 'Round: ';
+    choseDisplay.style.display = "block";
 }
 
-game();
 
 
